80c765edec686113d6f2954ed9bb80560107475d
Lauribee <laura_stroe_2001@yahoo.com>
2021-11-01 09:52:21 +0100

    oprettelse af pizza er mulig nu

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,16 @@
 public class Pizza {
+    private String pizzaName;
+    private int price;
+    private String ingredients;
+
+    public Pizza(String pizzaName, String ingredients, int price) {
+        this.pizzaName = pizzaName;
+        this.ingredients = ingredients;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return pizzaName + ": " + ingredients + "............................." + price + ",-";
+    }
 }
