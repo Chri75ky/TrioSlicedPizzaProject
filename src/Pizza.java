a211743b6895a8a48495487b669772ab387b7e53
Lauribee <laura_stroe_2001@yahoo.com>
2021-11-03 10:57:19 +0100

    Pizza er oprettede med ingredienser i PizzaMenu

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,11 +1,62 @@
+import java.util.ArrayList;
+
 public class Pizza {
     private String pizzaName;
     private int price;
-    private String ingredients;
+    Ingredient ingredient;
+    Ingredient ingredient1;
+    Ingredient ingredient2;
+    Ingredient ingredient3;
+    Ingredient ingredient4;
+    Ingredient ingredient5;
+    Ingredient ingredient6;
 
-    public Pizza(String pizzaName, String ingredients, int price) {
+    public Pizza(String pizzaName, Ingredient ingredient, int price) {
         this.pizzaName = pizzaName;
-        this.ingredients = ingredients;
+        this.ingredient = ingredient;
+        this.price = price;
+
+    }
+
+    public Pizza(String pizzaName, Ingredient ingredient, Ingredient ingredient1, Ingredient ingredient2, Ingredient ingredient3, int price) {
+        this.pizzaName = pizzaName;
+        this.ingredient = ingredient;
+        this.ingredient1 = ingredient1;
+        this.ingredient2 = ingredient2;
+        this.ingredient3 = ingredient3;
+        this.price = price;
+    }
+
+    public Pizza(String pizzaName, Ingredient ingredient, Ingredient ingredient1, Ingredient ingredient2, Ingredient ingredient3, Ingredient ingredient4, Ingredient ingredient5, int price) {
+        this.pizzaName = pizzaName;
+        this.ingredient = ingredient;
+        this.ingredient1 = ingredient1;
+        this.ingredient2 = ingredient2;
+        this.ingredient3 = ingredient3;
+        this.ingredient4 = ingredient4;
+        this.ingredient5 = ingredient5;
+        this.price = price;
+    }
+
+    public Pizza(String pizzaName, Ingredient ingredient, Ingredient ingredient1, Ingredient ingredient2, Ingredient ingredient3, Ingredient ingredient4, Ingredient ingredient5, Ingredient ingredient6, int price) {
+        this.pizzaName = pizzaName;
+        this.ingredient = ingredient;
+        this.ingredient1 = ingredient1;
+        this.ingredient2 = ingredient2;
+        this.ingredient3 = ingredient3;
+        this.ingredient4 = ingredient4;
+        this.ingredient5 = ingredient5;
+        this.ingredient6 = ingredient6;
+        this.price = price;
+    }
+
+    public Pizza(String pizzaName, Ingredient ingredient, Ingredient ingredient1, Ingredient ingredient2, Ingredient ingredient3, Ingredient ingredient4, int price) {
+        this.pizzaName = pizzaName;
+        this.ingredient = ingredient;
+        this.ingredient1 = ingredient1;
+        this.ingredient2 = ingredient2;
+        this.ingredient3 = ingredient3;
+        this.ingredient4 = ingredient4;
         this.price = price;
     }
 
@@ -22,7 +73,5 @@ public class Pizza {
         return price;
     }
 
-    public String getIngredients() {
-        return ingredients;
-    }
+
 }
